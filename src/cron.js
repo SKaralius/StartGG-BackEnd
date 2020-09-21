29effdaccf08d790f174dad6842c1dda8efbc100
SKaralius <k.simonas.k@gmail.com>
2020-09-21 21:51:19 +0300

    A space.

--- src/cron.js
+++ src/cron.js
@@ -3,7 +3,7 @@ import getData from "./util/functions.js";
 
 // Gets data every 29th and 59th minute
 // In other words, once an hour.
-cron.schedule("29, 59 * * * *", () => {
+cron.schedule("29,59 * * * *", () => {
   console.log("Refreshing data");
   getData();
 });
